a417bf6ef3f830fd8c66430af294e2a316fdfd05
sverg84 <134344913+sverg84@users.noreply.github.com>
2025-03-10 14:22:57 -0700

    remove dark mode, use single theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { SessionProvider } from "next-auth/react";
 import { UserMenu } from "@/components/auth/user-menu";
 import Link from "next/link";
 import NextTopLoader from "nextjs-toploader";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
@@ -24,26 +23,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <body className={quicksand.className}>
-        <ThemeProvider
-          attribute="class"
-          enableSystem={false}
-          disableTransitionOnChange={true}
-        >
-          <SessionProvider>
-            <ApolloProvider>
-              <NextTopLoader color="#ff7b54" height={6} showSpinner={false} />
-              <header className="border-b">
-                <div className="mx-auto flex items-center justify-between px-4 py-4 max-w-7xl">
-                  <Link href="/" className="text-2xl font-bold">
-                    KitchenKin
-                  </Link>
-                  <UserMenu />
-                </div>
-              </header>
-              {children}
-            </ApolloProvider>
-          </SessionProvider>
-        </ThemeProvider>
+        <SessionProvider>
+          <ApolloProvider>
+            <NextTopLoader color="#ff7b54" height={6} showSpinner={false} />
+            <header className="border-b">
+              <div className="mx-auto flex items-center justify-between px-4 py-4 max-w-7xl">
+                <Link href="/" className="text-2xl font-bold">
+                  KitchenKin
+                </Link>
+                <UserMenu />
+              </div>
+            </header>
+            {children}
+          </ApolloProvider>
+        </SessionProvider>
       </body>
     </html>
   );
